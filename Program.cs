c256537282fc85a2badc46828dcf83caf602944e
Charmipatel2271 <103488150+Charmipatel2271@users.noreply.github.com>
2022-04-20 23:22:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, i;
+            Console.WriteLine("Enter a 2 or more digit number");
+            number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                i = number % 10;
+                sum = sum + i;
+                number /= 10;
+            }
+            Console.WriteLine("sum=" + sum);
+            Console.ReadLine();
+        }
+    }
+}
